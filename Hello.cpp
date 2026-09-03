5ce1b3e9cebb02d8725facff89dae945c2761ccd
ingnic <ingnicchavez@gmail.com>
2026-09-02 22:35:17 -0700

    Update header info

---
+++ Hello.cpp
@@ -0,0 +1,11 @@
+// CSCI 27X - GitHub Upload Practice
+// Name: Nic Chavez
+// Section: MCSCI-271-1290
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello from GitHub! My first commit works." << endl;
+	return 0;
+}
